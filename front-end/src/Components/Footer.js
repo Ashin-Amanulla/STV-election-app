2b360e46add6fd4f8c317f28b853d576ec2f22f1
Tijo T Joseph <tijotjoseph@Tijos-MacBook-Air.local>
2023-03-31 13:43:47 +0530

    login page added

--- front-end/src/Components/Footer.js
+++ front-end/src/Components/Footer.js
@@ -2,18 +2,21 @@ import { FaFacebook,FaTwitter,FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer class="bg-white text-gray-700 text-center">
-  <div class="container mx-auto py-8 px-4 md:flex md:justify-between">
-    <div class="mb-6 md:mb-0 md:w-1/2">
-      <h2 class="text-lg font-bold mb-2">About Us</h2>
-      <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam cursus lacinia lectus, vel gravida est cursus eget.</p>
+    <footer className="bg-white text-gray-700 text-center">
+  <div className="container mx-auto py-8 px-4 md:flex md:justify-between">
+    <div className="mb-6 md:mb-0 md:w-1/2">
+      <h2 className="text-lg font-bold mb-2">About Us</h2>
+      <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam cursus lacinia lectus, vel gravida est cursus eget.</p>
+      <p className="text-center text-gray-500 text-xs">
+        &copy;2023 Name. All rights reserved.
+    </p>
     </div>
-    <div class="md:w-1/2">
-      <h2 class="text-lg font-bold mb-2">Follow Us</h2>
-      <div class="flex space-x-4 justify-center">
-        <a href="#" class="text-gray-700 hover:text-gray-300 text-2xl"><FaFacebook /></a>
-        <a href="#" class="text-gray-700 hover:text-gray-300 text-2xl"><FaTwitter /></a>
-        <a href="#" class="text-gray-700 hover:text-gray-300 text-2xl"><FaInstagram /></a>
+    <div className="md:w-1/2">
+      <h2 className="text-lg font-bold mb-2">Follow Us</h2>
+      <div className="flex space-x-4 justify-center">
+        <a href="#" className="text-gray-700 hover:text-gray-300 text-2xl"><FaFacebook /></a>
+        <a href="#" className="text-gray-700 hover:text-gray-300 text-2xl"><FaTwitter /></a>
+        <a href="#" className="text-gray-700 hover:text-gray-300 text-2xl"><FaInstagram /></a>
       </div>
     </div>
   </div>
--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitData = () => {
+    console.log({ email, password });
+    setEmail('')
+    setPassword('')
+  }
   return (
-    <div>login</div>
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-10 rounded-lg shadow-md w-3/5">
+        <h2 className="text-2xl font-bold mb-5 text-[#dc668a]">Login</h2>
+          <div className="mb-5">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-5">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <a className="inline-block align-baseline px-2 font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+            Forgot Password?
+          </a>
+          <button
+            className="bg-[#dc668a] hover:bg-[#dd265e] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type='submit'
+            onClick={submitData}
+          >
+            Login
+          </button>
+      </div>
+    </div>
+
   )
 }
 
