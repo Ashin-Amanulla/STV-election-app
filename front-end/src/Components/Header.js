4cef4593e13b8b6453818ec86ade324e4ea16d67
Tijo T Joseph <tijotjoseph@Tijos-MacBook-Air.local>
2023-03-31 08:00:24 +0530

    svg size and color change

--- front-end/src/Components/Header.js
+++ front-end/src/Components/Header.js
@@ -23,7 +23,7 @@ function Header() {
             className="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
             onClick={toggleMenu}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  stroke="currentColor" className="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  stroke="currentColor" className="w-10 h-10 text-white">
               <path
                 className={`${isOpen ? 'hidden' : 'block'}`}
                 d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5"
