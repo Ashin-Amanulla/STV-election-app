a9116555d356de9d46cc40eee3c78dbf2dc2d025
Tijo T Joseph <tijotjoseph003@gmail.com>
2023-04-05 13:26:51 +0530

    Script tag added

--- front-end/src/pages/_app.js
+++ front-end/src/pages/_app.js
@@ -1,5 +1,6 @@
 import Header from "@/Components/Header";
 import Head from "next/head";
+import Script from 'next/script'
 import "@/styles/globals.css";
 import Footer from "@/Components/Footer";
 
@@ -19,14 +20,14 @@ export default function App({ Component, pageProps }) {
           @import
           url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@500&display=swap');
         </style>
-        <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+      </Head>
+      <Script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></Script>
         <df-messenger
           intent="WELCOME"
           chat-title="STV"
           agent-id="4fef4c10-442f-4a9e-891b-5dddf4e24e0f"
           language-code="en"
         ></df-messenger>
-      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
