95254c8e82659963e1799fbbb28cbdeb33f2c117
hello-world-ttj <tijotjoseph003@gmail.com>
2023-04-11 20:39:49 +0530

    admin date declaration design change

--- front-end/src/pages/dashboard/declare-date.js
+++ front-end/src/pages/dashboard/declare-date.js
@@ -1,40 +1,48 @@
-import AdminMenu from '@/Components/AdminMenu'
-import React, { useState } from 'react'
-import moment from 'moment'
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
-const localizer = momentLocalizer(moment)
+import AdminMenu from "@/Components/AdminMenu";
+import React, { useState } from "react";
+import { DateRangePicker } from "rsuite";
 function Declaredate() {
-    const [events, setEvents] = useState([])
-
-    const handleSelect = ({ start, end }) => {
-      const title = window.prompt('Enter event title:')
-      if (title) {
-        setEvents([...events, { start, end, title }])
-      }
+  const [startDay, setStartDay] = useState();
+  const [endDay, setEndDay] = useState();
+  const setDay = (e) => {
+    if (e) {
+      setStartDay(e[0])
+      setEndDay(e[1])
+    } else {
+      setStartDay("")
+      setEndDay("")
     }
+
+  }
   return (
     <div className="flex flex-row h-screen">
-        <AdminMenu />
-        <div className="flex flex-col container m-4">
-        <h2 className="text-lg font-semibold mb-4 text-red-500 underline decoration-4">Declare the election date</h2>
+      <AdminMenu />
+      <div className="flex flex-col container m-4">
+        <h2 className="text-lg font-semibold mb-4 text-red-500 underline decoration-4">
+          Declare the election date
+        </h2>
         <div className="w-full max-w-7xl mx-auto p-4">
-      <Calendar
-        selectable
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-        onSelectSlot={handleSelect}
-      />
-    </div>
-          {events.map(e => {
-        console.log(e.start);
-    })}
+          <div className="field">
+            <DateRangePicker
+              format="yyyy-MM-dd hh:mm aa"
+              showMeridian
+              onChange={setDay}
+              defaultCalendarValue={[
+                new Date("2022-02-01 00:00:00"),
+                new Date("2022-05-01 23:59:59"),
+              ]}
+            />
+          </div>
+          {startDay && endDay && (
+        <p>
+          Selected date range:{' '}
+          {`${startDay} - ${endDay}`}
+        </p>
+      )}
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Declaredate
+export default Declaredate;
